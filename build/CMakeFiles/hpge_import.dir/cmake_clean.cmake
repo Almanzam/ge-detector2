570028fa49864cb6640276504a18596cd1515fc4
Mark Almanza <almanzam@vt.edu>
2017-06-07 15:55:23 -0400

    second commit, now using nano

---
+++ build/CMakeFiles/hpge_import.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hpge_import.dir/main.cpp.o"
+  "CMakeFiles/hpge_import.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/hpge_import.dir/src/PhysicsList.cc.o"
+  "CMakeFiles/hpge_import.dir/src/PrimaryGeneratorAction.cc.o"
+  "hpge_import.pdb"
+  "hpge_import"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hpge_import.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
