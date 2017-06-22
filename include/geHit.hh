656f889e377e19fbc107310edd7d1b3cf12d8110
Mark <almanzam@vt.edu>
2017-06-22 14:48:48 -0400

    Add geHit header file and class

---
+++ include/geHit.hh
@@ -0,0 +1,13 @@
+/// \file geHit.hh
+/// \brief Definition of the geHit class
+
+#ifndef geHit_h
+#define geHit_h 1
+
+#include "G4VHit.hh"
+#include "G4THitsCollection.hh"
+#include "G4Allocator.hh"
+#include "G4ThreeVector.hh"
+#include "tls.hh"
+
+
