9b14dfbb402b57744307163acc7d053993b78336
Mark Almanza <almanzam@vt.edu>
2017-06-26 14:05:10 -0400

    Beginning to implement sensitive detector, and fixing the lead shield

--- include/DetectorConstruction.hh
+++ include/DetectorConstruction.hh
@@ -12,47 +12,17 @@
 //#include "G4ThreeVector.hh"
 #include "G4VUserDetectorConstruction.hh"
 
+class G4GDMLParser;
+
 class DetectorConstruction: public G4VUserDetectorConstruction
 {
 public:
 
-    DetectorConstruction(G4VPhysicalVolume *setWorld = 0){
-        fWorld = setWorld;
-    }
-    virtual G4VPhysicalVolume *Construct()
-    {
-      return fWorld;
-    }
-    //~DetectorConstruction();
-
-    //G4VPhysicalVolume *Construct();
-
-//     void SetCADFilename(std::string name) {
-//         filename = name;
-//     };
-// 
-//     void SetCADFiletype(std::string type) {
-//         filetype = type;
-//     };
-
+    DetectorConstruction(const G4GDMLParser& parser);
+    virtual G4VPhysicalVolume *Construct();
+    virtual void ConstructSDandField();
 
 private:
-//     G4VSolid *world_solid;
-//     G4LogicalVolume *world_logical;
-//     G4VPhysicalVolume *world_physical;
-    G4VPhysicalVolume *fWorld;
-//     G4ThreeVector offset;
-//     G4ThreeVector cuvec;
-//     G4ThreeVector my_Foilvec;
-//     G4ThreeVector shellvec;
-//     G4ThreeVector al_foilvec;
-//     
-//     G4VSolid *cad_solid;
-//     G4LogicalVolume *cad_logical;
-//     G4VPhysicalVolume *cad_physical;
-// 
-//     std::string filename;
-//     std::string filetype;
-};
+    const G4GDMLParser& fParser;
 
 #endif
