f3b60418ef2c6e27c75e2f14610fb5722e37255a
Mark <almanzam@vt.edu>
2017-08-03 15:05:36 -0400

    lskdf

--- src/geEventAction.cc
+++ src/geEventAction.cc
@@ -108,7 +108,7 @@ void geEventAction::EndOfEventAction(const G4Event* event)
 //             totEnergyDetect += energyD;             
           }
 	  if(totEnergy > 0){
-		fhisto->FillHisto(0,totEnergy*CLHEP::keV);
+		fhisto->FillHisto(0,totEnergy / keV);
       }
     G4cout << "    "  
            << hc->GetSize() << " hits stored in this event" << G4endl;
