a72cd99df44a8e7cc0051a8aae0612fc9a1ebb48
Mark Almanza <Almanzam@vt.edu>
2019-02-20 19:27:25 -0500

    Add script to iterate through all angles in Co60 efficiency simulation

---
+++ scripts/Co_60_E.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mac=efficiency_Co60_X.mac
+count=12
+COUNTER=0
+         until [  $COUNTER -gt $count ]; do
+             ../build/ge-detector ../macros/${$mac/X/$count}
+             let COUNTER+=1
+         done
+
