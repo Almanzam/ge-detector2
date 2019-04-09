6f87d045d85772d676778db53d170d791fc6b20d
Mark Almanza <Almanzam@vt.edu>
2019-04-09 14:31:30 -0400

    typing on ubuntu is hard

--- Co60_cal/Co_60_E.sh
+++ Co60_cal/Co_60_E.sh
@@ -1,8 +1,8 @@
-#!/b=efficiency_Co60_X.mac
+#!/bin/bash
+mac=Co60_X.mac
 count=6
 COUNTER=0
          until [  $COUNTER -gt $count ]; do
              ../build/ge-detector ../macros/${mac/X/$COUNTER}
              let COUNTER+=1
          done
-
